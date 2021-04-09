4b5e8fff0b61fe5b82cc9420545344a1621ebd1b
davedissian <davedissian@hotmail.com>
2021-04-09 14:03:44 -0300

    Inicio da estilização

--- index.php
+++ index.php
@@ -4,13 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/main.css" type="text/css" rel="stylesheet"/>
     <title>DIVLOPERS></title>
 </head>
 <body>
-	<?php
-		require_once './backend/connection.php';
-		require_once 'menu.html';
-	?>
+<div class="bg-color size-box">
+			<header>
+				<nav>
+					<div class="margin-content menu">
+						<div id="logo">
+							<p> &lt;Divlopers&gt; </p>
+						</div>
+						<ul>
+						<li>
+							<a href="usuario.php"> Cadastrar Programador</a>
+						</li>
+
+						<li>
+							<a href="perfil.php"> Perfil </a>
+						</li>
+
+						<li>
+							<a id="nav-link" href="login.php"> Login </a>
+						</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+
+			<div class="margin-content">
+				<div class="heading">
+					<h1>
+						Bem-vindo ao Divlopers
+					</h1>
+					<div class="paragraph">
+						<p> A plataforma para DEVS </p>
+					</div>
+				</div>
+			</div>
+		</div>
 	<div class="container my-4">
 		<h3>Programadores</h3>
 
@@ -18,7 +50,7 @@
 			<p>Em obras...</p>
 		<h4>Em ordem alfabetica</h4>
 				<?php
-					
+					require_once './backend/connection.php';
 					$nomes = "SELECT * FROM usuario ORDER BY nome ASC";
 					$executa = mysqli_query($connect, $nomes);
 					while($dados = mysqli_fetch_array($executa)){
